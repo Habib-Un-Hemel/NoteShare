862d2f453a91bc1a1238149eb922ee0c95849db9
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-06-27 12:38:40 +0600

    Done with the basic express setup and connection also done to the database

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+const app = express();
+
+await connectDB();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running  on port ${PORT}`);
+});
+export default app;
