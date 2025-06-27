6568a7567081eeec9fae9ca1768d3be2d15e3516
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-06-27 12:38:40 +0600

    Done with the basic express setup and connection also done to the database

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("MongoDB connection established successfully.")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/NoteShare`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+export default connectDB;
