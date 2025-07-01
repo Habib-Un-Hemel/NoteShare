277c48b31403d293b02ddf64c933c2ea651fddd5
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-07-01 16:33:56 +0600

    API for blog list, details, delete and update

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -58,3 +58,56 @@ export const addBlog = async (req, res) => {
     });  
   }
 };
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+    res.json({ success: true, blogs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.params; // Assuming blogId comes from URL parameters
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.json({ success: false, message: "Blog not found" });
+    }
+    res.json({ success: true, blog });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}; 
+
+export const deleteBlogById = async (req, res) => {
+  try {
+    const { id } = req.body; 
+    await Blog.findByIdAndDelete(id);
+
+    res.json({ success: true, message: "Blog deleted successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}; 
+
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body; // Assuming the blog ID is sent in the request body
+    const blog = await Blog.findById(id);
+
+    // if (!blog) {
+    //   return res
+    //     .status(404)
+    //     .json({ success: false, message: "Blog not found" });
+    // }
+
+    blog.isPublished = !blog.isPublished; // Toggle the isPublished status
+    await blog.save(); // Save the updated blog
+
+    res.json({ success: true, message: "Blog status updated" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,23 +1,5 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-const uploadDir = path.join(process.cwd(), "uploads");
-
-// Ensure uploads directory exists
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
+const upload = multer({ storage: multer.diskStorage({}) });
 
 export default upload;
